015d24825720d149cc4c6da6c676b8fa384875c1
Chaitanya Atreya <atreya@gmail.com>
2014-03-06 23:26:04 +0000

    Added deploy on ec2 script

---
+++ deploy-on-ec2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Killing running Jetty"
+killall java
+
+echo "Pulling code from GitHub"
+cd ~/Projects/FEDU
+git pull
+echo "Building FEDU project"
+grails war
+cp ~/Projects/FEDU/target/FEDU-0.1.war ~/Programs/jetty/webapps/root.war
+
+echo "Starting Jetty server"
+cd ~/Programs/jetty
+rm nohup.out
+nohup java -jar start.jar &
+

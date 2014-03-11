f38c8ef7dce5207e72c9a55601454398fc188572
Chaitanya Atreya <atreya@gmail.com>
2014-03-11 00:15:30 -0700

    Refactor Bootstrap to use spring injection syntax correctly
    The Grails nomenclature is to use camel-cased service name (userService
    instead of UserService) to inject a service.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,26 +1,23 @@
 import fedu.Blog
-import fedu.BlogService
 import fedu.Project
-import fedu.ProjectService
-import fedu.UserService
 
 class BootStrap {
-	def BlogService
-    def UserService
-    def ProjectService
+    def blogService
+    def userService
+    def projectService
 
 	def init = { servletContext ->
         // Bootstrap users and roles
-        UserService.bootstrap()
+        userService.bootstrap()
 
 		// Check whether blogs already exist, and create otherwise.
 		if (Blog.count() == 0) {
-			BlogService.bootstrap()
+            blogService.bootstrap()
 		}
 
         // Check whether projects already exist, and create otherwise.
         if (Project.count() == 0) {
-            ProjectService.bootstrap()
+            projectService.bootstrap()
         }
 	}
 

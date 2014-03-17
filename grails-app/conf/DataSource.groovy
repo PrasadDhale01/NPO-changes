1a8e96b3ee67dbd1da071a4e36ecb9c17a1183e8
Chaitanya Atreya <atreya@gmail.com>
2014-03-17 01:25:03 -0700

    Set to dbCreate "create-drop" in prod, for now.
    Otherwise, have to delete the db files on disk for every schema change
    during heavy development. Not worth the pain.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -23,14 +23,16 @@ environments {
     }
     test {
         dataSource {
-            dbCreate = "update"
+            // dbCreate = "update"
+            dbCreate = "create-drop"
             url = "jdbc:h2:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             // url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
+            // dbCreate = "update"
+            dbCreate = "create-drop"
             url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
             properties {
                // Documentation for Tomcat JDBC Pool

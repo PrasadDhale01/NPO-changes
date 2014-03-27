93b99939493e767e3b372cba11949e8dc6416fc5
Chaitanya Atreya <atreya@gmail.com>
2014-03-26 23:32:21 -0700

    Update Google Analytics

--- web-app/js/ga.js
+++ web-app/js/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-48887980-1', '54.197.49.69');
+ga('create', 'UA-48887980-1', 'fedu.org');
 ga('send', 'pageview');

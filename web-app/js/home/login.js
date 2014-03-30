0b0b7787af28957f08b4a64ecab3d570339cff3f
Chaitanya Atreya <atreya@gmail.com>
2014-03-29 18:28:47 -0700

    Set jquery validate defaults in fedu.js
    Set query validate defaults in fed.’s at a global level, so that each
    js doesn’t have to set these defaults.

--- web-app/js/home/login.js
+++ web-app/js/home/login.js
@@ -11,21 +11,6 @@ $(function() {
             j_password: {
                 required: true
             }
-        },
-        highlight: function (element) {
-            $(element).closest('.form-group').addClass('has-error');
-        },
-        unhighlight: function (element) {
-            $(element).closest('.form-group').removeClass('has-error');
-        },
-        errorElement: 'span',
-        errorClass: 'help-block',
-        errorPlacement: function(error, element) {
-            if (element.parent('.input-group').length) {
-                error.insertAfter(element.parent());
-            } else {
-                error.insertAfter(element);
-            }
         }
     });
 });
--- web-app/js/project/create.js
+++ web-app/js/project/create.js
@@ -37,21 +37,6 @@ $(function() {
                 url: true
             }
             */
-        },
-        highlight: function (element) {
-            $(element).closest('.form-group').addClass('has-error');
-        },
-        unhighlight: function (element) {
-            $(element).closest('.form-group').removeClass('has-error');
-        },
-        errorElement: 'span',
-        errorClass: 'help-block',
-        errorPlacement: function(error, element) {
-            if (element.parent('.form-group').length) {
-                error.insertAfter(element.parent());
-            } else {
-                error.insertAfter(element);
-            }
         }
     });
 

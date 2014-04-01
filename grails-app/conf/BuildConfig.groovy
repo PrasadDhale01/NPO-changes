89db62ba3d5d6e17f9a7a65683a6f3c4abb5c0c6
Chaitanya Atreya <atreya@gmail.com>
2014-03-31 21:10:04 -0700

    Integrate quartz job scheduler
    Run something as a sample cron job once a day (just logs for now).

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -65,6 +65,7 @@ grails.project.dependency.resolution = {
         compile ":spring-security-core:2.0-RC2"
         compile "org.grails.plugins:spring-security-facebook:0.15.2-CORE2"
         compile ":stripe:2.6"
+        compile ':quartz:1.0.1'
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"

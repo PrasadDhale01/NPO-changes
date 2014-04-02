635288fe4eb946e5dc49b32f212283fe94d11fb3
Chaitanya Atreya <atreya@gmail.com>
2014-04-01 20:51:30 -0700

    Remove the test-only index method in SecuredController

--- grails-app/controllers/fedu/SecuredController.groovy
+++ grails-app/controllers/fedu/SecuredController.groovy
@@ -5,7 +5,4 @@ import grails.plugin.springsecurity.annotation.Secured
 @Secured(['ROLE_ADMIN'])
 class SecuredController {
 
-    def index() {
-        render 'In SecuredController.index'
-    }
 }

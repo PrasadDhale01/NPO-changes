ea31ad74a4ba9b890c9955da889f46d36f957fad
Chaitanya Atreya <atreya@gmail.com>
2014-04-06 11:58:56 -0700

    Switch to tomcat 7.0.52.1 (doesn't support https otherwise)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,7 +54,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.50.1"
+        build ":tomcat:7.0.52.1"
         // build ':jetty:2.0.3'
 
         // plugins for the compile step

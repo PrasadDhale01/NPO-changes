16dc3c574ab1678f0ae40da603984b854be7d31f
Chaitanya Atreya <atreya@gmail.com>
2014-04-06 17:38:20 -0700

    Don't kill Jetty till war is built
    Reduce down-time

--- deploy-on-ec2.sh
+++ deploy-on-ec2.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Killing running Jetty"
-killall java
-
 echo "Pulling code from GitHub"
 cd ~/Projects/FEDU
 git pull
 echo "Building FEDU project"
 grails war
 cp ~/Projects/FEDU/target/FEDU-0.1.war ~/Programs/jetty/webapps/root.war
+
+echo "Killing running Jetty"
+killall java
 
 echo "Starting Jetty server"
 cd ~/Programs/jetty

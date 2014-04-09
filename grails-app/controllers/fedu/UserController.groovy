357233bc99ee7ac30fb0a87114055fba6b6b0f33
Chaitanya Atreya <atreya@gmail.com>
2014-04-08 23:45:53 -0700

    Set admin profile path to /admin/profile
    That is, when user is admin, redirect /user/profile to /admin/profile

--- grails-app/controllers/fedu/UserController.groovy
+++ grails-app/controllers/fedu/UserController.groovy
@@ -6,11 +6,17 @@ import grails.plugin.springsecurity.annotation.Secured
 class UserController {
     def userService
 
+    @Secured(['ROLE_ADMIN'])
+    def adminprofile() {
+        User user = (User)userService.getCurrentUser()
+        render view: 'profile/admin/index', model: [user: user]
+    }
+
     @Secured(['IS_AUTHENTICATED_FULLY'])
     def profile() {
         User user = (User)userService.getCurrentUser()
         if (userService.isAdmin()) {
-            render view: 'profile/admin/index', model: [user: user]
+            redirect action: 'adminprofile'
         } else {
             def projects = Project.findAllByUser(user)
             def contributions = Contribution.findAllByUser(user)

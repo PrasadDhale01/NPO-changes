267d25410041c7d9463bfba4134f42be8d83054d
Chaitanya Atreya <atreya@gmail.com>
2014-04-11 00:49:29 -0700

    Create reward complete

--- grails-app/controllers/fedu/RewardController.groovy
+++ grails-app/controllers/fedu/RewardController.groovy
@@ -8,4 +8,10 @@ class RewardController {
     def list() {
         render (view: 'list/index', model: [rewards: Reward.list()])
     }
+
+    def save() {
+        new Reward(params).save(failOnError: true)
+        flash.message = 'Successfully created a new reward'
+        redirect action: 'list'
+    }
 }

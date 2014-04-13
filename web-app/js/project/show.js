a705e4e2cd0951ec79386d82847ffdef02660597
Chaitanya Atreya <atreya@gmail.com>
2014-04-12 17:02:30 -0700

    Stay on comments tab
    Make tabs survive a refresh.

--- web-app/js/project/show.js
+++ web-app/js/project/show.js
@@ -1,3 +1,14 @@
 $(function() {
-    $('.selectpicker').selectpicker();
+    console.log('show.js initialized');
+
+    var hash = window.location.hash;
+    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+    $('.nav a').click(function (e) {
+        $(this).tab('show');
+        var scrollmem = $('body').scrollTop();
+        window.location.hash = this.hash;
+        $('html,body').scrollTop(scrollmem);
+    });
+
 });

7fb15fef4de5e9428752b1fbe37efff652aa0b2a
Chaitanya Atreya <atreya@gmail.com>
2014-04-15 16:20:15 +0000

    Update to deploy script

--- deploy-on-ec2.sh
+++ deploy-on-ec2.sh
@@ -10,8 +10,12 @@ cp ~/Projects/FEDU/target/FEDU-0.1.war ~/Programs/jetty/webapps/root.war
 echo "Killing running Jetty"
 killall java
 
+JETTY_HOME="/home/ubuntu/Programs/jetty"
+JAVA_OPTIONS="-XX:MaxPermSize=256m -Xms1024m -Xmx2048m"
+NEWRELIC_OPTIONS=-javaagent:$JETTY_HOME/newrelic/newrelic.jar
 echo "Starting Jetty server"
-cd ~/Programs/jetty
+cd $JETTY_HOME
 rm nohup.out
 # nohup java -jar start.jar &
-nohup java -Xms1024m -Xmx2048m -javaagent:/home/ubuntu/Programs/jetty/newrelic/newrelic.jar -jar start.jar --module=https,deploy &
+nohup java $JAVA_OPTIONS $NEWRELIC_OPTIONS -jar start.jar --module=https,deploy &
+

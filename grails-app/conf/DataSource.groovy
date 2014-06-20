3f2fb35c3d2637272a73bec6a1062c0bd79dd5dd
Chaitanya Atreya <atreya@gmail.com>
2014-06-20 01:48:17 -0700

    Undo create-drop from prod

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -35,7 +35,6 @@ environments {
     production {
         dataSource {
             // dbCreate = "update"
-            dbCreate = "create-drop"
             url = "jdbc:mysql://fedudbbeta.cvblatijndiw.us-east-1.rds.amazonaws.com:3306/fedudb"
             driverClassName = "com.mysql.jdbc.Driver"
             dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"

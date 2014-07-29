b51e3ea000c6cb0ab7016bd78f56188ebe6ed807
PRASADDHALE <pdakshay88@gmail.com>
2014-07-29 19:18:53 +0530

    Update RewardController.groovy

--- grails-app/controllers/fedu/RewardController.groovy
+++ grails-app/controllers/fedu/RewardController.groovy
@@ -11,7 +11,7 @@ class RewardController {
 
     def save() {
         new Reward(params).save(failOnError: true)
-        flash.message = 'Congratulations! A new reward has been created successfully'
+        flash.message = 'Successfully created a new reward'
         redirect action: 'list'
     }
 }

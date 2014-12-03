f8d759ef6aafd56b6a5d7b75c89d8f3e3c71a8c6
krishnaSahu <kumarkrishnasahu@gmail.com>
2014-12-02 23:42:27 -0800

    'Digits only' not shown when we press enter

--- web-app/js/fund/fund.js
+++ web-app/js/fund/fund.js
@@ -43,7 +43,7 @@ $(function() {
         //called when key is pressed in textbox
         $("#amount").keypress(function (e) {
             //if the letter is not digit then display error and don't type anything
-            if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            if (e.which != 8 && e.which != 0 && e.which !=13 && (e.which < 48 || e.which > 57)) {
                 //display error message
                 $("#errormsg").html("Digits Only").show().fadeOut("slow");
             return false;

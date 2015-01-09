d6beda73926f66ba325578048801b8e19b8601f8
Tushar-warhade <tushar14830@gmail.com>
2015-01-09 19:29:27 +0530

    CDRA-450-DELETE-OPTION
    Delete image option for already added organisation logo and campaign
    images.

--- grails-app/controllers/fedu/FundController.groovy
+++ grails-app/controllers/fedu/FundController.groovy
@@ -132,6 +132,19 @@ class FundController {
             return
         }
 
+        def totalContribution= contributionService.getTotalContributionForProject(project)
+        def contPrice = params.double(('amount'))
+        def amt =project.amount
+        def reqAmt=(999/100)*amt
+        def remainAmt=reqAmt- totalContribution
+        def percentage=((totalContribution + contPrice)/ amt)*100
+        if(percentage>999)
+        {
+            flash.sentmessage= "Amount should not exceed more than \$"+remainAmt
+             render view: 'fund/index', model: [project: project, user:user]
+            
+        }
+        else{
         if (project && reward) {
             if (project.paypalEmail){
                 payByPaypal(params,project,reward,user)
@@ -143,6 +156,7 @@ class FundController {
             }
         } else {
             render view: 'error', model: [message: 'This project or reward does not exist. Please try again.']
+        }
         }
     }
     def acknowledge() {

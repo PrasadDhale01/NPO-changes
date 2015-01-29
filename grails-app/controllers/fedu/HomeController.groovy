fc732c7125acd0c67cd11d0e9365959daec99522
PRASADDHALE <pdakshay88@gmail.com>
2015-01-29 12:16:47 +0530

    Updated the code on Home controller and change the id for vote for austin with the election navigator
    Updated the code on Home controller and change the id for vote for
    austin with the election navigator

--- grails-app/controllers/fedu/HomeController.groovy
+++ grails-app/controllers/fedu/HomeController.groovy
@@ -4,7 +4,13 @@ class HomeController {
 	def projectService
 
     def index() {
-    	def projects = projectService.projectOnHomePage()
-        return [projects: projects]
+		def base_url = grailsApplication.config.crowdera.BASE_URL
+		if(base_url.contains("localhost") || base_url.contains("staging")){
+			def projects = projectService.showProjects()
+			return [projects: projects]
+		}else{
+			def projects = projectService.projectOnHomePage()
+			return [projects: projects]
+		}
     }
 }

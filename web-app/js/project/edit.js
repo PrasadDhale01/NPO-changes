175399422a6e27f956455b469c24a098fc894eb5
Tushar-warhade <tushar14830@gmail.com>
2015-02-11 17:47:27 +0530

    CDRA-556-DECIMAL-ROUND-OFF-NEW

--- web-app/js/project/edit.js
+++ web-app/js/project/edit.js
@@ -136,6 +136,15 @@ $(function() {
           }
      });
 
+     //called when key is pressed in textbox
+     $("#amount").keypress(function (e) {
+            //if the letter is not digit then display error and don't type anything
+            if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                //display error message
+                $("#errormsg").html("Digits Only").show().fadeOut("slow");
+            return false;
+        } 
+     });
      $.validator.addMethod('isWebUrl', function(value, element){
          if(value && value.length !=0){
             var p= /(?:(?:www):\/\/)?(?:www.)\/?/;

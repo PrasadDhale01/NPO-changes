468f0b50829c7be20455d88363a3a14155379c5c
Chaitanya Atreya <chai@sparsa.com>
2015-04-08 20:54:25 -0700

    Added a bunch of very useful scripts
    Added a bunch of very useful scripts

---
+++ scripts/restart-tomcat-on-prod-ec2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+NEWRELIC_HOME="/home/ubuntu/newrelic"
+NEWRELIC_JAR=$NEWRELIC_HOME/newrelic.jar
+NEWRELIC_OPTS=-javaagent:$NEWRELIC_JAR
+TOMCAT_HOME="/home/ubuntu/Programs/tomcat"
+CROWDERA_HOME="/home/ubuntu/Projects/Crowdera"
+export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=256m -Xms1024m -Xmx2048m $NEWRELIC_OPTS"
+
+
+echo "Stop tomcat"
+cd $TOMCAT_HOME
+./bin/catalina.sh stop -force
+
+rm -rf $TOMCAT_HOME/webapps/ROOT
+cp $CROWDERA_HOME/target/Crowdera-0.1.war $TOMCAT_HOME/webapps/ROOT.war
+
+echo "Starting tomcat server"
+cd $TOMCAT_HOME
+./bin/startup.sh
+
---
+++ scripts/restart-tomcat-on-staging-ec2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+TOMCAT_HOME="/home/ubuntu/Programs/tomcat"
+export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=256m -Xms1024m -Xmx2048m"
+
+echo "Stop tomcat"
+cd $TOMCAT_HOME
+./bin/catalina.sh stop -force
+
+echo "Starting tomcat server"
+cd $TOMCAT_HOME
+./bin/startup.sh
+

e83bc7b28421d6addeb513633fa960b5d20859ef
Chaitanya Atreya <chai@sparsa.com>
2015-04-08 20:58:35 -0700

    One more doc file

---
+++ scripts/deploy-jetty-on-staging-ec2.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Pulling code from GitHub"
+cd ~/Projects/Crowdera
+git checkout production
+git pull
+echo "Building Crowdera project"
+grails test war
+echo "Copying Crowdera war to Jetty"
+ls -l ~/Projects/Crowdera/target
+cp ~/Projects/Crowdera/target/Crowdera-0.1.war ~/Programs/jetty/webapps/root.war
+
+echo "Killing running Jetty"
+killall java
+
+JETTY_HOME="/home/ubuntu/Programs/jetty"
+JAVA_OPTIONS="-XX:MaxPermSize=256m -Xms1024m -Xmx2048m"
+
+echo "Starting Jetty server"
+cd $JETTY_HOME
+rm nohup.out
+nohup java $JAVA_OPTIONS -jar start.jar --module=https,deploy &
+

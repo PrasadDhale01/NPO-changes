f5cd9ec12a7cf15bd0c69ac8bb1cd568295013c3
Chaitanya Atreya <chai@sparsa.com>
2015-04-20 09:57:20 -0700

    Add killall java to all deployment scripts
    Without this, tomcat has some lingering processes.

--- scripts/restart-tomcat-on-prod-ec2.sh
+++ scripts/restart-tomcat-on-prod-ec2.sh
@@ -11,6 +11,7 @@ export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=256m -Xms1024m -Xmx2048m $NEWRELIC_
 echo "Stop tomcat"
 cd $TOMCAT_HOME
 ./bin/catalina.sh stop -force
+killall java
 
 rm -rf $TOMCAT_HOME/webapps/ROOT
 cp $CROWDERA_HOME/target/Crowdera-0.1.war $TOMCAT_HOME/webapps/ROOT.war

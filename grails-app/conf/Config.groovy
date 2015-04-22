a6a25c838cda57136705fea408e7cd3e44a5b363
Tushar-warhade <tushar@crowdera.co>
2015-04-22 18:14:15 +0530

    Anonymous-issue-on-contribution-fixed

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -182,7 +182,7 @@ environments {
         grails.plugin.springsecurity.facebook.appId = '${crowdera.facebook.appId}'
         grails.plugin.springsecurity.facebook.secret = '${crowdera.facebook.secret}'
 
-        crowdera.BASE_URL = 'http://crowdera.co'
+        crowdera.BASE_URL = 'https://crowdera.co'
 
         grails.logging.jul.usebridge = false
         grails.dbconsole.enabled = true

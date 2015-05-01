98b0df0a0c51f01fd5b6e83040f9a0f643562ae2
Chaitanya Atreya <chai@sparsa.com>
2015-05-01 15:52:03 -0700

    Split script directory to prod/test/staging

--- scripts/prod/deploy-jetty-on-prod-ec2.sh
+++ scripts/prod/deploy-jetty-on-prod-ec2.sh
@@ -5,19 +5,26 @@ cd ~/Projects/Crowdera
 git checkout production
 git pull
 echo "Building Crowdera project"
-grails test war
+grails prod war
 echo "Copying Crowdera war to Jetty"
-ls -l ~/Projects/Crowdera/target
 cp ~/Projects/Crowdera/target/Crowdera-0.1.war ~/Programs/jetty/webapps/root.war
 
 echo "Killing running Jetty"
 killall java
 
+
 JETTY_HOME="/home/ubuntu/Programs/jetty"
 JAVA_OPTIONS="-XX:MaxPermSize=256m -Xms1024m -Xmx2048m"
+NEWRELIC_JAR=$JETTY_HOME/newrelic/newrelic.jar
+NEWRELIC_OPTIONS=-javaagent:$NEWRELIC_JAR
+
+# Inform New Relic of a deployment
+# curl -H "x-api-key:d9bd0ead04e2cd242884c8009232ecbec39578a118fec0e" -d "deployment[app_name]=Crowdera" https://api.newrelic.com/deployments.xml
+#java -jar $NEWRELIC_JAR deployment
 
 echo "Starting Jetty server"
 cd $JETTY_HOME
 rm nohup.out
-nohup java $JAVA_OPTIONS -jar start.jar --module=https,deploy &
+# nohup java -jar start.jar &
+nohup java $JAVA_OPTIONS $NEWRELIC_OPTIONS -jar start.jar --module=https,deploy &
 
---
+++ scripts/staging/restart-tomcat-on-staging-ec2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TOMCAT_HOME="/home/ubuntu/Programs/tomcat"
+
+echo "Stop tomcat"
+cd $TOMCAT_HOME
+./bin/catalina.sh stop -force
+killall java
+
+echo "Starting tomcat server"
+cd $TOMCAT_HOME
+./bin/startup.sh

86910825d3b52db27fe78eff79669768d6b4cad2
Chaitanya Atreya <chai@sparsa.com>
2015-05-07 10:37:51 -0700

    Update scripts with catalina opts

--- scripts/staging/deploy-tomcat-on-staging-ec2.sh
+++ scripts/staging/deploy-tomcat-on-staging-ec2.sh
@@ -2,6 +2,8 @@
 
 TOMCAT_HOME="/home/ubuntu/Programs/tomcat"
 CROWDERA_HOME="/home/ubuntu/Projects/Crowdera"
+CATALINA_OPTS="-d64 -Xms2048M -Xmx2048M -XX:NewSize=1024m -XX:MaxNewSize=1024m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+DisableExplicitGC"
+export CATALINA_OPTS
 
 echo "Pulling code from GitHub"
 cd $CROWDERA_HOME
@@ -15,6 +17,7 @@ cd $TOMCAT_HOME
 ./bin/catalina.sh stop -force
 killall java
 
+rm -rf $TOMCAT_HOME/logs/*
 rm -rf $TOMCAT_HOME/webapps/ROOT
 cp $CROWDERA_HOME/target/Crowdera-0.1.war $TOMCAT_HOME/webapps/ROOT.war
 

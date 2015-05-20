609b5a78339aa0e7dc4ab2cbafd1dd815bcafe3f
kartikis26 <kartikis26@gmail.com>
2015-05-20 12:01:42 +0530

    CDRA-855 Bunch of equals and tostring issue

--- grails-app/domain/crowdera/Contribution.groovy
+++ grails-app/domain/crowdera/Contribution.groovy
@@ -1,5 +1,11 @@
 package crowdera
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@ToString(includeNames = true, includeFields = true, excludes = 'dateAndTime')
+@EqualsAndHashCode
+
 class Contribution {
 
     double amount

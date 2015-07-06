e7df6578b28a7e0325c1547e1d3c4c2b0f5ffbe2
kartikis26 <kartikis26@gmail.com>
2015-07-06 19:33:53 +0530

    Indentation issue corrected

--- web-app/js/project/create.js
+++ web-app/js/project/create.js
@@ -187,11 +187,11 @@ $(function() {
     $.validator.addMethod('isPaypalEmailVerified', function (value, element) {
         var ack = $("#paypalEmailAck").val();
         var base_url = $("#b_url").val();
- 	    if (base_url != 'https://crowdera.co'){
+        if (base_url != 'https://crowdera.co'){
             if(ack == 'Failure') {
                 return (ack == 'Success') ? ack : false;
             }
- 	    }
+        }
         return true;
     }, "Please enter verified paypal email id");
     
@@ -737,24 +737,24 @@ function setTitleText(){
     
    $('#paypalEmailId').change(function(){
 	   var base_url = $("#b_url").val();
-	   if (base_url != 'https://crowdera.co'){
-	       var email =  $('#paypalEmailId').val();
-	       $.ajax({
+       if (base_url != 'https://crowdera.co'){
+           var email =  $('#paypalEmailId').val();
+           $.ajax({
                type:'post',
                url:$("#b_url").val()+'/project/paypalEmailVerification',
                data:'email='+email,
                success: function(data){
                    $('#paypalEmailAck').val(data);
                    if (data == 'Success') {
-            	       $('.paypalVerification').find("span").remove();
-            	       $('.paypalVerification').closest(".form-group").removeClass('has-error');
+                       $('.paypalVerification').find("span").remove();
+                       $('.paypalVerification').closest(".form-group").removeClass('has-error');
                    }
                }
            }).error(function(){
-       	       alert('An error occured');
+               alert('An error occured');
            });
-	   }
-   });
+        }
+    });
 
 /*Javascript error raised due to tooltip is resolved*/
     /* Show pop-over tooltip on hover for some fields. */

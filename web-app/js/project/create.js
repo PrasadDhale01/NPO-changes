01cb3688b988c1cd468d5e02b8a63fc795a5ff95
Tushar-warhade <tushar@crowdera.co>
2015-08-21 18:55:05 +0530

    Safari and create page design issues fixed

--- web-app/js/project/create.js
+++ web-app/js/project/create.js
@@ -1,6 +1,8 @@
 $(function() {
     console.log("create.js initialized");
     
+    $('#iconfile').val('');
+    
     var rewardIteratorCount = $('#rewardCount').val();
     if (rewardIteratorCount > 0){
     	$('#rewardTemplate').show();
@@ -694,8 +696,6 @@ $(function() {
                });
                // Read the image
                picReader.readAsDataURL(file);
-               $('.projectImageFilediv, .createOrgIconDiv').find("span").remove();
-               $('.projectImageFilediv, .createOrgIconDiv').closest(".form-group").removeClass('has-error');
 	        }
 	    } 
     });

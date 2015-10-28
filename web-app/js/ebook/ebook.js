f6bc68741bb118a63cf60b207ae8c3747796f4d4
Tushar-warhade <tushar@crowdera.co>
2015-10-28 19:55:14 +0530

    Indentation resolved

--- web-app/js/ebook/ebook.js
+++ web-app/js/ebook/ebook.js
@@ -12,8 +12,8 @@ $(function() {
 		$(window).scroll(sticky_relocate);
 		sticky_relocate();
 	});
-	
-	$("#fbshare").click(function(){
+
+    $("#fbshare").click(function(){
         var url = 'http://www.facebook.com/sharer.php?p[url]='+ encodeURIComponent($('#fbShareUrl').val());
         window.open(url, 'Share on FaceBook', 'left=20,top=20,width=600,height=500,toolbar=0,menubar=0,scrollbars=0,location=0,resizable=1');
         return false;

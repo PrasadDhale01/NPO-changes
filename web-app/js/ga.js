b9aefde154f2a3c3bed206ed8a8f6c5dc63efbdf
prbharadwaj <priyapramod2012@gmail.com>
2016-12-21 14:17:29 -0800

    updating the google analytics code

--- web-app/js/ga.js
+++ web-app/js/ga.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     var currentEnv=$('#currentEnvironment').val();
     if(currentEnv === 'production'){
 
-    	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    	  ga('create', 'UA-56708048-4', 'auto');
-    	  ga('send', 'pageview');
+    		  ga('create', 'UA-56708048-2', 'auto');
+    		  ga('send', 'pageview');
 
     }else if (currentEnv === 'prodIndia'){
 

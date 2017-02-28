4d2bd7e25731d29d58a0660c35d189f71ad9bc26
krishnaSahu <kumarkrishnasahu@gmail.com>
2017-03-01 00:28:32 +0530

    campaign filteration added

--- grails-app/services/crowdera/ProjectService.groovy
+++ grails-app/services/crowdera/ProjectService.groovy
@@ -26,6 +26,7 @@ import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.DefaultHttpClient
+import org.grails.datastore.mapping.query.Query.IsNotNull;
 import org.hibernate.Session
 import org.hibernate.SessionFactory
 import org.hibernate.criterion.Projections;
@@ -6158,6 +6159,7 @@ class ProjectService {
             eq("draft", false)
             eq("rejected", false)
             eq("inactive", false)
+			isNotNull("citrusEmail")
         }
         
         def raised;
